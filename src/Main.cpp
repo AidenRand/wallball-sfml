57ab85fd8415ee8d29707a012f59512de5d62e84
aiden <aidenrphotos@gmail.com>
2023-05-05 05:50:38 -0400

    Add README.md

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,41 +1,26 @@
-#include "Platform/Platform.hpp"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	util::Platform platform;
-
-#if defined(_DEBUG)
-	std::cout << "Hello World!" << std::endl;
-#endif
-
-	sf::RenderWindow window;
-	// in Windows at least, this must be called before creating the window
-	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
-	// Use the screenScalingFactor
-	window.create(sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor), "SFML works!");
-	platform.setIcon(window.getSystemHandle());
-
-	sf::CircleShape shape(window.getSize().x / 2);
-	shape.setFillColor(sf::Color::White);
-
-	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("content/sfml.png");
-	shape.setTexture(&shapeTexture);
-
-	sf::Event event;
+	sf::RenderWindow window(sf::VideoMode(700, 900), "Wallball");
+	window.setFramerateLimit(30);
+	sf::Clock clock;
+	float dt;
 
 	while (window.isOpen())
 	{
+		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
+			{
 				window.close();
+			}
 		}
-
+		dt = clock.restart().asSeconds();
+		std::cout << dt << "\n";
 		window.clear();
-		window.draw(shape);
 		window.display();
 	}
-
 	return 0;
 }
